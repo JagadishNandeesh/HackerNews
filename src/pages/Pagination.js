2da11e70e9c5fd65100a7f89190e80be50cd9a4a
jagadish_n <jagadishthunders@gmail.com>
2020-06-08 19:00:24 +0530

    changing pagination colors

--- src/pages/Pagination.js
+++ src/pages/Pagination.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export const Pagination = ({ id }) => (
   <div className="paginator">
-    <span>
+    <span className={"prev"}>
       <Link to={`/${parseInt(id, 10) - 1}`}>Prev</Link>
     </span>
 
-    <span>
+    <span className={"next"}>
       <Link to={`/${parseInt(id, 10) + 1}`}>Next</Link>
     </span>
   </div>
