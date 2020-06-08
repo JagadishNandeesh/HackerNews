9405784f300aac4bb678feb53bf74c1aca202eda
jagadish_n <jagadishthunders@gmail.com>
2020-06-08 20:57:16 +0530

    css changes for header

--- src/pages/News.js
+++ src/pages/News.js
@@ -6,10 +6,18 @@ export const News = ({ news, id, toggle, upvote }) => {
   return (
     <div className={"gridcontainer"}>
       <div className="row header">
-        <div className="col-md-1 col-sm-1 col-xs-1">Comments</div>
-        <div className="col-md-1 col-sm-1 col-xs-1">Vote count</div>
-        <div className="col-md-1 col-sm-1 col-xs-1">UpVote</div>
-        <div className="col-md-9 col-sm-9 col-xs-9">New Details</div>
+        <div className="col-md-1 col-sm-1 col-xs-1 header-css-override">
+          Comments
+        </div>
+        <div className="col-md-1 col-sm-1 col-xs-1 header-css-override">
+          Vote count
+        </div>
+        <div className="col-md-1 col-sm-1 col-xs-1 header-css-override">
+          UpVote
+        </div>
+        <div className="col-md-9 col-sm-9 col-xs-9 header-css-override">
+          News Details
+        </div>
       </div>
 
       {news &&
@@ -35,7 +43,7 @@ export const News = ({ news, id, toggle, upvote }) => {
                     className={"triangle"}
                   ></div>
                 </div>
-                <div className="col-md-9 col-sm-9 col-xs-9 news-item-title">
+                <div className="col-md-9 col-sm-9 col-xs-9 news-item-title ">
                   <span data-testid="news-item-title">{news.title}</span>
                   <span className="hostName">
                     {news.url ? extractHostname(news.url) : ""}
